c7f078c012e8b4428e878472d2ddeb03e4c43f49
arnavmittal14 <144770918+arnavmittal14@users.noreply.github.com>
2024-07-18 15:45:01 -0600

    Update University.java

--- Exercise_C/ca/ucalgary/ensf380/University.java
+++ Exercise_C/ca/ucalgary/ensf380/University.java
@@ -4,14 +4,16 @@ public class University {
 	
     public static void main(String[] args) {
     	
-        Address address = new Address("2500 University Dr NW", "Calgary", "AB", "T2N 1N4", "Canada");
-        Professor professor = new Professor("Professor Mahdi", "1234567890", "mahdi.jaberzadehansa@ucalgary.ca", address, "ICT 114", 101000.00);
-        Student student = new Student("Arnav Mittal", "0123456789", "arnav.mittal1@ucalgary.ca", address, "S456", professor, 75.5);
+        Address professorAddress = new Address("2500 University Dr NW", "Calgary", "AB", "T2N 1N4", "Canada");
+        Address studentAddress = new Address("123 Street SW", "Calgary", "AB", "T3E 7N9", "Canada");
+        Professor professor = new Professor("Professor Mahdi", "1234567890", "mahdi.jaberzadehansa@ucalgary.ca", professorAddress, "ICT 114", 101000.00);
+        Student student = new Student("Arnav Mittal", "0123456789", "arnav.mittal1@ucalgary.ca", studentAddress, "S456", professor, 75.5);
 
         System.out.println("Professor Information:");
         System.out.println("Name: " + professor.getName());
         System.out.println("Teacher Number: " + professor.getTeacherNumber());
         System.out.println("Address: " + professor.getAddress().outputAsLabel());
+        System.out.println("Salary: "+ professor.getSalary());
 
         System.out.println("\nStudent Information:");
         System.out.println("Name: " + student.getName());
